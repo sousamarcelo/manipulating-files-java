e08d372782eea0db0367c32c8c2515f30612e65e
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2023-09-16 00:36:56 -0300

    file lines read successfully

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,40 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import mode.entitie.Product;
+
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {	
+		Locale.setDefault(Locale.US);
+		String path = "C:\\temp\\file.csv";
+		List<Product> products = new ArrayList<Product>();
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(path))){
+			String line = br.readLine();
+			
+			while (line != null) {
+				String[] vect = line.split(",");	
+				products.add(new Product(vect[0], Double.parseDouble(vect[1]), Integer.parseInt(vect[2])));
+				line = br.readLine();
+			}
+			
+			//for each provisario, conferindo se objetos foram criados na lista, copiados do arquivo.
+			for(Product product : products){
+				System.out.println(product.getName() + "," + product.getPrice() + "," + product.getQuantity());
+			}
+			
+			
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		
 
 	}
 
